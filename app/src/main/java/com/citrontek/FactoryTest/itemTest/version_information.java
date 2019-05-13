b1e3783ac107d13a2235b126570943661062fa7a
kkkkkn <1185553557@qq.com>
2019-05-13 18:02:27 +0800

    项目初始化

---
+++ app/src/main/java/com/citrontek/FactoryTest/itemTest/version_information.java
@@ -0,0 +1,15 @@
+package com.citrontek.FactoryTest.itemTest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.citrontek.FactoryTest.R;
+
+public class version_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_version_information);
+    }
+}
