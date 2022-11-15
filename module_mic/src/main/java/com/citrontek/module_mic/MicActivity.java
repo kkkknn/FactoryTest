7ec848c5dfc6162ae136f136ba6d5133d3e47c91
kkkkkn <1185553557@qq.com>
2022-11-15 15:25:21 +0800

    麦克风喇叭模块更新

--- module_mic/src/main/java/com/citrontek/module_mic/MicActivity.java
+++ module_mic/src/main/java/com/citrontek/module_mic/MicActivity.java
@@ -3,6 +3,7 @@ package com.citrontek.module_mic;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.appcompat.widget.AppCompatSeekBar;
 import androidx.appcompat.widget.AppCompatTextView;
 
 import android.media.MediaPlayer;
@@ -13,6 +14,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.kkkkkn.lib_common.router.CommonRouterConstant;
@@ -26,12 +28,13 @@ public class MicActivity extends AppCompatActivity {
     private AppCompatImageButton btn_start,btn_stop,btn_playback;
     private AppCompatButton btn_horn_test1;
     private AppCompatTextView tv_tip_record,tv_tip_play;
-    private MediaRecorder mediaRecorder;
-    private MediaPlayer mediaPlayer;
+    private final MediaRecorder mediaRecorder=new MediaRecorder();
+    private final MediaPlayer mediaPlayer=new MediaPlayer();
+    private AppCompatTextView tv_play_this_time,tv_play_finish_time;
     private File audioFile;
     private final String TAG="MicActivity";
 
-    private Handler handler=new Handler(Looper.getMainLooper()){
+    private final Handler handler=new Handler(Looper.getMainLooper()){
         public void dispatchMessage(android.os.Message msg) {
             if(msg.what==0){
                 tv_tip_record.setText("正在录音");
@@ -53,6 +56,8 @@ public class MicActivity extends AppCompatActivity {
                 btn_start.setEnabled(true);
                 btn_stop.setEnabled(false);
                 btn_playback.setEnabled(true);
+            }else if(msg.what==1000){
+                tv_play_this_time.setText(ShowTime(msg.arg1));
             }
         };
     };
@@ -75,21 +80,58 @@ public class MicActivity extends AppCompatActivity {
         tv_tip_record=findViewById(R.id.tv_tip_record);
         tv_tip_play=findViewById(R.id.tv_tip_play);
 
+        tv_play_finish_time=findViewById(R.id.play_finish_time);
+        tv_play_this_time=findViewById(R.id.play_this_time);
+
+
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Init_recording();
-                Log.i(TAG, "run: 初始化完成");
-                Start_recording();
-                Log.i(TAG, "run: 开始录音");
+                new Thread(){
+                    @Override
+                    public void run() {
+                        mediaRecorder.reset();
+                        // 第1步：设置音频来源（MIC表示麦克风）
+                        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                        //第2步：设置音频输出格式（默认的输出格式）
+                        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+                        //第3步：设置音频编码方式（默认的编码方式）
+                        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+
+                        //创建一个临时的音频输出文件
+                        try {
+                            if(audioFile==null){
+                                audioFile = File.createTempFile("record_", ".amr");
+                            }
+                            //第4步：指定音频输出文件
+                            mediaRecorder.setOutputFile(audioFile.getAbsolutePath());
+                            //第5步：调用prepare方法
+                            mediaRecorder.prepare();
+                            Log.i(TAG, "run: 初始化完成");
+                            mediaRecorder.start();
+                            handler.sendEmptyMessage(0);
+                            Log.i(TAG, "run: 开始录音");
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+
             }
         });
 
         btn_stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Stop_recording();
-                Log.i(TAG, "run: 结束录音");
+                new Thread(){
+                    @Override
+                    public void run() {
+                        mediaRecorder.stop();
+                        handler.sendEmptyMessage(1);
+                        Log.i(TAG, "run: 结束录音");
+                    }
+                }.start();
             }
         });
 
@@ -101,72 +143,7 @@ public class MicActivity extends AppCompatActivity {
 
             }
         });
-    }
 
-    /**
-     * 初始录音---设置录音格式，输出目录
-     */
-    private void Init_recording(){
-        mediaRecorder = new MediaRecorder();
-        // 第1步：设置音频来源（MIC表示麦克风）
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        //第2步：设置音频输出格式（默认的输出格式）
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
-        //第3步：设置音频编码方式（默认的编码方式）
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-
-        //创建一个临时的音频输出文件
-        try {
-            audioFile = File.createTempFile("record_", ".amr");
-            //第4步：指定音频输出文件
-            mediaRecorder.setOutputFile(audioFile.getAbsolutePath());
-            //第5步：调用prepare方法
-            mediaRecorder.prepare();
-
-            // 开启另一个线程获取实时最大音量   mediaRecorder.getMaxAmplitude();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    /**
-     * 开始录音
-     */
-    private void Start_recording(){
-        //调用start方法开始录音
-        Message msg = handler.obtainMessage();
-        msg.what = 0;
-        handler.sendMessage(msg);
-        mediaRecorder.start();
-    }
-
-    /**
-     *  停止录音
-     */
-    private void Stop_recording(){
-        //调用stop方法停止录音
-        Message msg = handler.obtainMessage();
-        msg.what = 1;
-        handler.sendMessage(msg);
-        mediaRecorder.stop();
-    }
-
-    /**
-     * 播放录音-
-     */
-    private void Test_recording(){
-        Message msg = handler.obtainMessage();
-        msg.what = 2;
-        handler.sendMessage(msg);
-        mediaPlayer = new MediaPlayer();
-        try {
-            mediaPlayer.setDataSource(audioFile.getAbsolutePath());
-            mediaPlayer.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        mediaPlayer.start();
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
@@ -176,5 +153,69 @@ public class MicActivity extends AppCompatActivity {
             }
         });
 
+
     }
+
+
+    /**
+     * 播放录音-
+     */
+    private void Test_recording(){
+        handler.sendEmptyMessage(2);
+        try {
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(audioFile.getAbsolutePath());
+            mediaPlayer.prepare();
+
+            mediaPlayer.start();
+            int Alltime= mediaPlayer.getDuration();
+            tv_play_finish_time.setText("/"+ShowTime(Alltime));
+            new Thread(){
+                @Override
+                public void run() {
+                    while (mediaPlayer.isPlaying()) {
+                        try {
+                            int CurrentPosition = mediaPlayer.getCurrentPosition();
+                            Message message=handler.obtainMessage();
+                            message.what=1000;
+                            message.arg1=CurrentPosition;
+                            handler.sendMessage(message);
+                            Thread.sleep(200);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaRecorder!=null){
+            mediaRecorder.stop();
+            mediaRecorder.release();
+        }
+        if(mediaPlayer!=null){
+            if(mediaPlayer.isPlaying()){
+                mediaPlayer.stop();
+            }
+            mediaPlayer.release();
+        }
+    }
+
+    public String ShowTime(int time){
+        time/=1000;
+        int minute=time/60;
+        int hour=minute/60;
+        int second=time%60;
+        minute%=60;
+        return String.format("%02d:%02d", minute, second);
+    }
+
 }
